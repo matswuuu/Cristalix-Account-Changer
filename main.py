dc2b8c27f98fb7de77773d74d2df6b12a9237690
matswuuu <matswuuu@gmail.com>
2023-06-05 16:35:13 +1000

    fixed

--- main.py
+++ main.py
@@ -79,78 +79,6 @@ def start(self, button):
 
 
 class Scrollable_Frame(customtkinter.CTkScrollableFrame):
-    class Group_Frame(customtkinter.CTkScrollableFrame):
-        def __init__(self, master):
-        super().__init__(master)
-        self.grid_columnconfigure(0, weight=1)
-        self.configure(width=560, height=100,
-                       border_color="#ffffff", border_width=1)
-        self.nick_entrys = []
-        self.token_entrys = []
-        self.start_buttons = []
-        self.delete_buttons = []
-        self.row = 1
-
-        self.name_entry = customtkinter.CTkEntry(
-            self, placeholder_text="Имя группы", width=160, height=20,
-            border_color="#ffffff", border_width=1)
-        self.name_entry.grid(row=0, column=0, pady=(10, 0), padx=(5, 5))
-
-        self.start_all_button = customtkinter.CTkButton(
-            self, text="Запустить все", width=100, height=20, state=state,
-            command=lambda: threading.Thread(target=start_all, args=(self,)).start())
-        self.start_all_button.grid(row=0, column=2, pady=(10, 0), padx=(5, 5))
-
-        self.add_button = customtkinter.CTkButton(
-            self, text="+", width=20, height=20, command=self.add)
-        self.add_button.grid(row=0, column=3, pady=(10, 0), padx=(10, 5))
-
-    def add(self, nick="", token=""):
-        nick_entry = customtkinter.CTkEntry(
-            self, placeholder_text="Ник", width=180, height=25)
-        nick_entry.grid(row=self.row, column=0, pady=(10, 0), padx=(10, 10))
-        if nick != "":
-            nick_entry.insert(0, nick)
-        self.nick_entrys.append(nick_entry)
-
-        token_entry = customtkinter.CTkEntry(
-            self, placeholder_text="Токен", width=230, height=25)
-        token_entry.grid(row=self.row, column=1, pady=(10, 0), padx=(10, 10))
-        if token != "":
-            token_entry.insert(0, token)
-        self.token_entrys.append(token_entry)
-
-        start_button = customtkinter.CTkButton(
-            self, text="Запустить", width=100, height=25, state=state,
-            command=lambda: start(self, start_button))
-        start_button.grid(row=self.row, column=2, pady=(10, 0), padx=(10, 10))
-        self.start_buttons.append(start_button)
-
-        delete_button = customtkinter.CTkButton(
-            self, text="-", width=20, height=25, command=lambda: self.delete(delete_button))
-        delete_button.grid(row=self.row, column=3, pady=(10, 0), padx=(10, 5))
-        self.delete_buttons.append(delete_button)
-
-        self.row = self.row + 1
-
-    def delete(self, button):
-        id = self.delete_buttons.index(button)
-        if len(self.nick_entrys) == 1:
-            group_id = groups.index(self)
-            groups.pop(group_id)
-            
-            self.grid_forget()
-
-        self.nick_entrys[id].grid_forget()
-        self.token_entrys[id].grid_forget()
-        self.start_buttons[id].grid_forget()
-        self.delete_buttons[id].grid_forget()
-
-        self.nick_entrys.pop(id)
-        self.token_entrys.pop(id)
-        self.start_buttons.pop(id)
-        self.delete_buttons.pop(id)
-
     
     def __init__(self, master):
         super().__init__(master)
@@ -212,13 +140,57 @@ class Scrollable_Frame(customtkinter.CTkScrollableFrame):
         self.start_buttons.pop(id)
         self.delete_buttons.pop(id)
 
+class Group_Frame(Scrollable_Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.configure(width=560, height=100,
+                    border_color="#ffffff", border_width=1)
+        self.nick_entrys = []
+        self.token_entrys = []
+        self.start_buttons = []
+        self.delete_buttons = []
+        self.row = 1
+
+        self.name_entry = customtkinter.CTkEntry(
+            self, placeholder_text="Имя группы", width=160, height=20,
+            border_color="#ffffff", border_width=1)
+        self.name_entry.grid(row=0, column=0, pady=(10, 0), padx=(5, 5))
+
+        self.start_all_button = customtkinter.CTkButton(
+            self, text="Запустить все", width=100, height=20, state=state,
+            command=lambda: threading.Thread(target=start_all, args=(self,)).start())
+        self.start_all_button.grid(row=0, column=2, pady=(10, 0), padx=(5, 5))
+
+        self.add_button = customtkinter.CTkButton(
+            self, text="+", width=20, height=20, command=self.add)
+        self.add_button.grid(row=0, column=3, pady=(10, 0), padx=(10, 5))
+
+    def delete(self, button):
+        id = self.delete_buttons.index(button)
+        if len(self.nick_entrys) == 1:
+            group_id = groups.index(self)
+            groups.pop(group_id)
+            
+            self.grid_forget()
+
+        self.nick_entrys[id].grid_forget()
+        self.token_entrys[id].grid_forget()
+        self.start_buttons[id].grid_forget()
+        self.delete_buttons[id].grid_forget()
+
+        self.nick_entrys.pop(id)
+        self.token_entrys.pop(id)
+        self.start_buttons.pop(id)
+        self.delete_buttons.pop(id)
+
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.title("ACCOUNT CHANGER by matswuuu")
         self.geometry("650x430")
-        #self.iconbitmap("images/logo.ico")
+        self.iconbitmap("images/logo.ico")
 
         self.frame = Scrollable_Frame(self)
         self.frame.place(x=10, y=70)
